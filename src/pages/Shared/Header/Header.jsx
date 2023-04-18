38e95c034b318b071f3dfedb69927483edd76b24
kabirshuvo <kabirshuvo19@gmail.com>
2023-04-19 02:29:38 +0600

    NavBar Added

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,9 +1,11 @@
 import moment from 'moment';
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Navbar from 'react-bootstrap/Navbar';
 import Marquee from "react-fast-marquee";
-
-
 const Header = () => {
     return (
         <Container>
@@ -18,6 +20,40 @@ const Header = () => {
                 <Marquee speed={200}>
   I can be a React component, multiple React components, or just some text.
 </Marquee>
+           </div>
+
+           <div>
+           <Navbar bg="light" expand="lg">
+      <Container fluid>
+        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1">Home</Nav.Link>
+            <Nav.Link href="#action2">Link</Nav.Link>
+            <NavDropdown title="Link" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">
+                Something else here
+              </NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#" disabled>
+              Link
+            </Nav.Link>
+          </Nav>
+          <img src="" alt="UserImg" />
+          <Button variant="outline-success">Search</Button>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
            </div>
         </Container>
     );
