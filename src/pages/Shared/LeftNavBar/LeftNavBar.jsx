7dbdb1ed85c0f2b2a3b52259b1fa42c4c309edb1
kabirshuvo <kabirshuvo19@gmail.com>
2023-04-25 04:07:01 +0600

    server created and category data fetched from server

--- src/pages/Shared/LeftNavBar/LeftNavBar.jsx
+++ src/pages/Shared/LeftNavBar/LeftNavBar.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const LeftNavBar = () => {
+
+    const [categories, setCategories] = useState([]);
+
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/categories`)
+        .then(res => res.json())
+        .then(data => setCategories(data))
+        .catch(error => console.log(error))
+    }, [])
+
     return (
         <div>
-            <h3>Left Nav Bar</h3>
+            <h3>All Categories</h3>
+
+       <div className='ps-4 py-2'>
+       {
+            categories.map(category => <h6 key={category.id}>
+
+                <Link className='text-decoration-none'>{category.name}</Link>
+            </h6>)
+        }
+
+       </div>
         </div>
     );
 };
