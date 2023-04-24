e872ac1be85941206a9636b92971addb086487de
kabirshuvo <kabirshuvo19@gmail.com>
2023-04-25 02:13:40 +0600

    Right NavBar Created

---
+++ src/pages/Shared/QZone/Qzone.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+// import qZone1 from '../../../assets/qZone1';
+// import qZone2 from '../../../assets/qZone2';
+// import qZone3 from '../../../assets/qZone3';
+
+const Qzone = () => {
+    return (
+        <>
+        <div className='bg-secondary text-center'>
+            <h3>Q-Zone</h3>
+
+            <div>
+                <img style={{width: '15rem', marginTop: '1rem', borderRadius: '1rem'}} src='https://images.unsplash.com/photo-1440288736878-766bd5839edb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8a2lkcyUyMHBsYXlpbmd8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60' alt="Q-Zone-1-Img" />
+                <img style={{width: '15rem', marginTop: '1rem', borderRadius: '1rem'}} src='https://images.unsplash.com/photo-1607453998774-d533f65dac99?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGtpZHMlMjBwbGF5aW5nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60' alt="Q-Zone-2-Img" />
+                <img style={{width: '15rem', marginTop: '1rem', borderRadius: '1rem'}} src='https://images.unsplash.com/photo-1517164850305-99a3e65bb47e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGtpZHMlMjBwbGF5aW5nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60' alt="Q-Zone-3-Img" />
+            </div>
+
+            <h3>...</h3>
+
+           
+            
+        </div>
+
+<div>
+<img style={{width: '19rem', marginTop: '1rem', borderRadius: '1rem'}} src="https://images.unsplash.com/photo-1614914135224-925593607248?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fG5hdHVyZSUyMGJhY2tncm91bmR8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt="" />
+</div>
+
+</>
+
+
+    );
+};
+
+export default Qzone;
--- src/pages/Shared/RightNavBar/RightNavBar.jsx
+++ src/pages/Shared/RightNavBar/RightNavBar.jsx
@@ -1,26 +1,32 @@
 import React from 'react';
 import { Button, ListGroup } from 'react-bootstrap';
-import { FaFacebook, FaGithub, FaGoogle, FaLinkedinIn } from 'react-icons/fa';
+import { FaFacebook, FaGithub, FaGoogle, FaGooglePlus, FaInstagram, FaLinkedinIn, FaTwitter } from 'react-icons/fa';
+import Qzone from '../QZone/Qzone';
 
 const RightNavBar = () => {
     return (
-        <div>
+    <div>
             <h3>LogIn With</h3>
         <div className='d-flex flex-column gap-4'  aria-label="LogIn">
                 <Button variant="primary"><FaGoogle/> LogIn With Google</Button>
                 <Button variant="secondary"> <FaGithub/> LogIn With GitHub</Button>
         </div>
-<div>
-    <h3 className='mt-5'>Follow Us On</h3>
-    <ListGroup>
-      <ListGroup.Item> <FaFacebook></FaFacebook> FaceBook</ListGroup.Item>
-      <ListGroup.Item> <FaLinkedinIn></FaLinkedinIn> Linked-In</ListGroup.Item>
-      <ListGroup.Item>Twiter</ListGroup.Item>
-      <ListGroup.Item>InstaGrame</ListGroup.Item>
-      <ListGroup.Item>GooglePlus</ListGroup.Item>
-    </ListGroup>
-</div>
+        <div>
+            <h3 className='mt-5'>Follow Us On</h3>
+            <ListGroup>
+                    <ListGroup.Item> <FaFacebook></FaFacebook> FaceBook</ListGroup.Item>
+                    <ListGroup.Item> <FaLinkedinIn></FaLinkedinIn> Linked-In</ListGroup.Item>
+                    <ListGroup.Item> <FaTwitter></FaTwitter> Twiter</ListGroup.Item>
+                    <ListGroup.Item> <FaInstagram></FaInstagram> InstaGrame</ListGroup.Item>
+                    <ListGroup.Item> <FaGooglePlus></FaGooglePlus> GooglePlus</ListGroup.Item>
+            </ListGroup>
         </div>
+
+       <h3 className='mt-5'> The Q Zone Section</h3>
+
+        <Qzone></Qzone>
+
+    </div>
     );
 };
 
