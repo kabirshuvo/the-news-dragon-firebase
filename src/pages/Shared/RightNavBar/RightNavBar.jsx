694e091e9c366607c772b0c0861d616105fda978
kabirshuvo <kabirshuvo19@gmail.com>
2023-04-25 01:30:29 +0600

    index.html updated, listgroup added in Right nav bar

--- src/pages/Shared/RightNavBar/RightNavBar.jsx
+++ src/pages/Shared/RightNavBar/RightNavBar.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
-import { FaBeer, FaFacebook, FaGithub, FaGoogle, FaTiktok, } from 'react-icons/fa';
+import { Button, ListGroup } from 'react-bootstrap';
+import { FaFacebook, FaGithub, FaGoogle, FaLinkedinIn } from 'react-icons/fa';
 
 const RightNavBar = () => {
     return (
         <div>
-            <h3>Right Nav Bar <FaBeer/></h3>
+            <h3>LogIn With</h3>
         <div className='d-flex flex-column gap-4'  aria-label="LogIn">
                 <Button variant="primary"><FaGoogle/> LogIn With Google</Button>
                 <Button variant="secondary"> <FaGithub/> LogIn With GitHub</Button>
         </div>
-
-        <div className='d-flex flex-column gap-2 mt-5'  aria-label="Social">
-                <Button variant="primary"> <FaFacebook/> FaceBook</Button>
-                <Button variant="secondary">    Twiter</Button>
-                <Button variant="secondary">  <FaTiktok/> Tiktok</Button>
-        </div>
+<div>
+    <h3 className='mt-5'>Follow Us On</h3>
+    <ListGroup>
+      <ListGroup.Item> <FaFacebook></FaFacebook> FaceBook</ListGroup.Item>
+      <ListGroup.Item> <FaLinkedinIn></FaLinkedinIn> Linked-In</ListGroup.Item>
+      <ListGroup.Item>Twiter</ListGroup.Item>
+      <ListGroup.Item>InstaGrame</ListGroup.Item>
+      <ListGroup.Item>GooglePlus</ListGroup.Item>
+    </ListGroup>
+</div>
         </div>
     );
 };
