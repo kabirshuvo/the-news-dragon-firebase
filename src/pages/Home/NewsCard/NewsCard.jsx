1985b81a91f1ad850c80d808674d66c8f22ed1b2
kabirshuvo <kabirshuvo19@gmail.com>
2023-04-25 19:07:09 +0600

    News Card Initiated

--- src/pages/Home/NewsCard/NewsCard.jsx
+++ src/pages/Home/NewsCard/NewsCard.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
+import { Card } from 'react-bootstrap';
 
 const NewsCard = ({news}) => {
     const {_id, title, details, thumbnail_url, image_url} = news;
 
     return (
-        <div>
-            <p>{title}</p>
-            <p>{details}</p>
-        </div>
+        <Card>
+        <Card.Header>{title}</Card.Header>
+        <Card.Img variant="top" src={image_url} />
+        <Card.Body>
+          <blockquote className="blockquote mb-0">
+            <p>
+              {' '}
+              {details}
+              {' '}
+            </p>
+            <footer className="blockquote-footer">
+              Someone famous in <cite title="Source Title">Source Title</cite>
+            </footer>
+          </blockquote>
+        </Card.Body>
+      </Card>
     );
 };
 
