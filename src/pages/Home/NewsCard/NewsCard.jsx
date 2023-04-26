995ca479201b439564e3a70b428e9befb0c00850
kabirshuvo <kabirshuvo19@gmail.com>
2023-04-26 21:13:15 +0600

    Card Header img resized, dated formated

--- src/pages/Home/NewsCard/NewsCard.jsx
+++ src/pages/Home/NewsCard/NewsCard.jsx
@@ -1,21 +1,33 @@
+import moment from 'moment/moment';
 import React from 'react';
-import { Button, Card, Image } from 'react-bootstrap';
+import { Card, Image } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const NewsCard = ({news}) => {
-    const {_id, title, details, image_url, author} = news;
+    const {_id, title, details, image_url, author,} = news;
 
     return (
-        <Card>
+        <Card >
              
-        <Card.Header><Image style={{width: '3rem'}} src={author.img} roundedCircle /> {author.name}</Card.Header>
-        <Card.Header>{title}</Card.Header>
+        <Card.Header className='d-flex'>
+          
+            <Image style={{height: '3rem'}} src={author?.img} roundedCircle /> 
+        
+            <div className='d-flex flex-column ps-2'>
+                <p className='mb-0'>{author?.name}</p>
+                <p > {moment(author?.published_date).format('Do MMM YYYY')}</p>
+            </div>
+        
+        
+        </Card.Header>
         <Card.Img variant="top" src={image_url} />
+        <Card.Header>{title}</Card.Header>
+        
         <Card.Body>
           <blockquote className="blockquote mb-0">
             <p>
               {' '}
-              {details.length < 250 ? <>{details}</> : <>{details.slice(0, 250)} ... <Link to={`/news/${_id}`}><Button>Read More</Button></Link></>}
+              {details.length < 250 ? <>{details}</> : <>{details.slice(0, 250)} ... <Link to={`/news/${_id}`}><>Read More</></Link></>}
               {' '}
             </p>
             <footer className="blockquote-footer">
@@ -23,6 +35,7 @@ const NewsCard = ({news}) => {
             </footer>
           </blockquote>
         </Card.Body>
+        <Card.Footer>{title}</Card.Footer>
       </Card>
     );
 };
